d1a79870f6e5602b4dad35dfdf3ac7efb3179c1d
JullianP1 <jullianp1@learn.cssd.ab.ca>
2018-09-11 14:08:57 -0600

    fixed error

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -38,7 +38,7 @@ class ViewController: UIViewController {
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
-        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
 }
